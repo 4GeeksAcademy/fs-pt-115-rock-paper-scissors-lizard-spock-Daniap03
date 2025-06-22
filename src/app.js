039f609642f01e5c259c910d3cde9b3e4baf0b69
daniap03 <danielaunonpardo@gmail.com>
2025-06-22 18:54:07 +0000

    juego

--- src/app.js
+++ src/app.js
@@ -1,3 +1,38 @@
 //write your code here
-console.log("Hello Rigo from the console!");
+window.onload = function name() {
 
+    const elements = ["rock", "paper", "scissors", "lizard", "spock"]
+
+    const rules = {
+        rock : ['scissors','lizard'],
+        paper : ['rock', 'spock'],
+        scissors :['paper','lizard'],
+        lizard : ['spock','paper'],
+        spock : ['scissors', 'rock']
+
+    };
+
+    const getRandomElements = () => {
+        let index = Math.floor(Math.random() * elements.length)
+        return elements[index]
+    }
+    const checkElementsGuess = (userGuess) => {
+        const actualElement = getRandomElements()
+
+        console.log(`Tu eliges ${userGuess}`);
+        console.log(`La maquina elige ${actualElement}`);
+
+        if (userGuess === actualElement) {
+            console.log("Empate");
+        }
+        else if (rules[userGuess].includes(actualElement)){
+             console.log(`Ganaste!! ${userGuess} vence a ${actualElement}`);
+        }
+        else { 
+            console.log(`Perdsite... ${actualElement} vence a ${userGuess}`);
+        }
+
+    }
+    const guess = prompt ("Escoge el elemento: rock, paper, scissors, lizard, spock").toLowerCase()
+    checkElementsGuess(guess)
+}
