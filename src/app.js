4e04572f91c903e40d5fc19ffdc54b0dcc66823f
daniap03 <danielaunonpardo@gmail.com>
2025-06-25 18:29:48 +0000

    revision de proyectos

--- src/app.js
+++ src/app.js
@@ -4,14 +4,13 @@ window.onload = function name() {
     const elements = ["rock", "paper", "scissors", "lizard", "spock"]
 
     const rules = {
-        rock : ['scissors','lizard'],
-        paper : ['rock', 'spock'],
-        scissors :['paper','lizard'],
-        lizard : ['spock','paper'],
-        spock : ['scissors', 'rock']
+        rock: ['scissors', 'lizard'],
+        paper: ['rock', 'spock'],
+        scissors: ['paper', 'lizard'],
+        lizard: ['spock', 'paper'],
+        spock: ['scissors', 'rock']
 
     };
-
     const getRandomElements = () => {
         let index = Math.floor(Math.random() * elements.length)
         return elements[index]
@@ -23,16 +22,13 @@ window.onload = function name() {
         console.log(`La maquina elige ${actualElement}`);
 
         if (userGuess === actualElement) {
-            console.log("Empate");
+            return console.log("Empate");
         }
-        else if (rules[userGuess].includes(actualElement)){
-             console.log(`Ganaste!! ${userGuess} vence a ${actualElement}`);
+        if (rules[userGuess].includes(actualElement)) {
+            return console.log(`Ganaste!! ${userGuess} vence a ${actualElement}`);
         }
-        else { 
-            console.log(`Perdsite... ${actualElement} vence a ${userGuess}`);
-        }
-
+        console.log(`Perdsite... ${actualElement} vence a ${userGuess}`);
     }
-    const guess = prompt ("Escoge el elemento: rock, paper, scissors, lizard, spock").toLowerCase()
+    const guess = prompt("Escoge el elemento: rock, paper, scissors, lizard, spock").toLowerCase()
     checkElementsGuess(guess)
 }
